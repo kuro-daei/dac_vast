4cd44ad30a59e1c0aa8f2c32c6b4e9fbd2dbde0e
FlexOne推進部 近江　泰輔 <taisuke-oomi@dac.co.jp>
2017-06-01 10:42:47 +0900

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,31 +12,32 @@
 
 ActiveRecord::Schema.define(version: 20170531051840) do
 
-  create_table "campaigns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.integer  "limit_start"
-    t.string   "movie_url"
-    t.datetime "deleted_at"
+  create_table "Campaigns_Cuepoints", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "cuepoint_id", null: false
+    t.integer  "campaign_id", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["deleted_at"], name: "index_campaigns_on_deleted_at", using: :btree
+    t.datetime "deleted_at"
+    t.index ["campaign_id"], name: "index_campaigns_cuepoints_on_campaign_id", using: :btree
+    t.index ["cuepoint_id"], name: "index_campaigns_cuepoints_on_quepoint_id", using: :btree
   end
 
-  create_table "campaigns_cuepoints", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "cuepoint_id", null: false
-    t.integer "campaign_id", null: false
-    t.index ["campaign_id"], name: "index_campaigns_cuepoints_on_campaign_id", using: :btree
-    t.index ["cuepoint_id"], name: "index_campaigns_cuepoints_on_cuepoint_id", using: :btree
+  create_table "campaigns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",        null: false
+    t.datetime "start_at",    null: false
+    t.datetime "end_at",      null: false
+    t.integer  "limit_start", null: false
+    t.string   "movie_url",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.datetime "deleted_at"
   end
 
   create_table "cuepoints", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.datetime "deleted_at"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_cuepoints_on_deleted_at", using: :btree
+    t.datetime "deleted_at"
   end
 
   create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -50,4 +51,6 @@ ActiveRecord::Schema.define(version: 20170531051840) do
     t.index ["cuepoint_id"], name: "index_results_on_cuepoint_id", using: :btree
   end
 
+  add_foreign_key "Campaigns_Cuepoints", "campaigns"
+  add_foreign_key "Campaigns_Cuepoints", "cuepoints"
 end
